08ea229a3b3965375e6a653b127057b4b2f3e71c
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-11-15 12:32:40 +0200

    structure About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,9 @@
 import s from './About.module.scss';
-import InfoList from './InfoList/InfoList';
+import AboutMe from './AboutMe/AboutMe';
 
 const About = () => (
   <div className={s.about}>
-    <header>
-      <h4>About <span>Me</span></h4>
-    </header>
-    <div className={s.content}>
-      <div className={s.description}>
-        <p>
-          I am Khrystyna Rolska, I studied at Beetroot Academy. There I studied at the 
-          Front-End Developer course. I got experience to learn something new fast, I 
-          had teamwork and presentation practice.
-        </p>
-      </div>
-      <InfoList />
-    </div>
+    <AboutMe />
   </div>
 )
 
