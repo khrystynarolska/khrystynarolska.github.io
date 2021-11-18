e566b290d5b0aabc42cb8eed72e646ec34f8a59b
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-11-18 11:08:53 +0200

    component Education and scrolling for the  component About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,13 @@
 import s from './About.module.scss';
 import AboutMe from './AboutMe/AboutMe';
 import Skills from './Skills/Skills';
+import Education from './Education/Education';
 
 const About = () => (
   <div className={s.about}>
     <AboutMe />
     <Skills />
+    <Education />
   </div>
 )
 
---
+++ src/components/About/Education/Education.jsx
@@ -0,0 +1,33 @@
+import s from './Education.module.scss';
+import Wrapper from '../Wrapper/Wrapper';
+
+const institutions = [
+  {
+    institution: 'Khmelnytsky National University',
+    specialty: 'Specialty - personnel management and labor economics',
+    trainingPeriod: '2015-2020'
+  },
+  {
+    institution: 'Beetroot Academy',
+    specialty: 'Specialty - Front-End Developer',
+    trainingPeriod: 'June 2021 - October 2021'
+  }
+]
+
+const Education = () => {
+  return (
+    <div className={s.education}>
+      <Wrapper title={<>My <span>education</span></>}>
+        {institutions.map((i, idx) => (
+          <div className={s.content} key={idx}>
+            <h4>{i.institution}</h4>
+            <p>{i.specialty}</p>
+            <span>{i.trainingPeriod}</span>
+          </div>
+        ))}
+    </Wrapper>
+    </div>
+  )
+}
+
+export default Education
