5dc5bb4925493130422590ad60b34098c9c1962f
khrystynarolska <kristinarolskaya2017@gmail.com>
2021-11-29 14:25:02 +0200

    Bhromaon in component Projects

--- src/components/Works/Projects/Projects.jsx
+++ src/components/Works/Projects/Projects.jsx
@@ -1,22 +1,30 @@
 import s from './Projects.module.scss';
 import Project from './Project/Project';
 
-import imgLeospa from './assets/Leospa.png';
+import imgBhromaon from './assets/Bhromaon.png'
 import imgMonticello from './assets/Monticello.png'
+import imgLeospa from './assets/Leospa.png';
 
 const myProjects = [
   {
+    src: imgBhromaon,
+    lable: 'Bhromaon',
+    technologies: 'HTML, CSS',
+    value: 'This is my first project. I wrote it with help HTML and CSS',
+    href: ' https://khrystynarolska.github.io/bhromaon/'
+  },
+  {
     src: imgMonticello,
     lable: 'Monticello',
-    technologies: 'HTML, SCSS, JacaScript',
-    value: 'This is an vue project. This is a Leospa project with well-known components (hamburger menu, scroll to, shapes, tabs, etc.).',
+    technologies: 'HTML, SCSS, JavaScript',
+    value: 'This project is implemented with HTML, SCSS and JavaScript (scroll, slow scroll, slider, hamburger menu)',
     href: ' https://khrystynarolska.github.io/monticello/'
   },
   {
     src: imgLeospa,
     lable: 'Leospa',
     technologies: 'Vue js',
-    value: 'This is an vue project. This is a Leospa project with well-known components (hamburger menu, scroll to, shapes, tabs, etc.).',
+    value: 'This is an vue project. This is a Leospa project with well-known components (hamburger menu, scroll to, shapes, tabs, etc.)',
     href: 'https://khrystynarolska.github.io/leospa-vue-project/'
   }
 ]
